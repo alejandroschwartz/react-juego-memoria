5b0265690a2f3820575d9539a492a42f654b62d2
Schwartz, Alejandro <alejandromatiassch@gmail.com>
2020-10-19 12:51:11 -0300

    Updates

---
+++ precache-manifest.32e6ec3c821c3784b1df5c279853b78c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff4fc2449752c22b498ae107769c791",
+    "url": "/react-juego-memoria/index.html"
+  },
+  {
+    "revision": "73d78bc2bd9ecb8573f4",
+    "url": "/react-juego-memoria/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ec1844c4a55e9542734",
+    "url": "/react-juego-memoria/static/js/2.73005641.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-juego-memoria/static/js/2.73005641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d78bc2bd9ecb8573f4",
+    "url": "/react-juego-memoria/static/js/main.38f82f4d.chunk.js"
+  },
+  {
+    "revision": "19ae667af9df90f19920",
+    "url": "/react-juego-memoria/static/js/runtime-main.8498e3fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-juego-memoria/static/media/logo.5d5d9eef.svg"
+  }
+]);
